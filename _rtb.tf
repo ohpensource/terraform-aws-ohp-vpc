abdf5e4fc87723882c4e29ffc924a642b21a2feb
mliner <mliner@users.noreply.github.com>
2021-11-22 13:13:32 +0100

    major: Updated structrure (#2)
    * updated structure

---
+++ _rtb.tf
@@ -0,0 +1,328 @@
+################################################################################
+# Default route
+################################################################################
+
+resource "aws_default_route_table" "default" {
+  count = var.create_vpc && var.manage_default_route_table ? 1 : 0
+
+  default_route_table_id = aws_vpc.this[0].default_route_table_id
+  propagating_vgws       = var.default_route_table_propagating_vgws
+
+  dynamic "route" {
+    for_each = var.default_route_table_routes
+    content {
+      # One of the following destinations must be provided
+      cidr_block      = route.value.cidr_block
+      ipv6_cidr_block = lookup(route.value, "ipv6_cidr_block", null)
+
+      # One of the following targets must be provided
+      egress_only_gateway_id    = lookup(route.value, "egress_only_gateway_id", null)
+      gateway_id                = lookup(route.value, "gateway_id", null)
+      instance_id               = lookup(route.value, "instance_id", null)
+      nat_gateway_id            = lookup(route.value, "nat_gateway_id", null)
+      network_interface_id      = lookup(route.value, "network_interface_id", null)
+      transit_gateway_id        = lookup(route.value, "transit_gateway_id", null)
+      vpc_endpoint_id           = lookup(route.value, "vpc_endpoint_id", null)
+      vpc_peering_connection_id = lookup(route.value, "vpc_peering_connection_id", null)
+    }
+  }
+
+  timeouts {
+    create = "5m"
+    update = "5m"
+  }
+
+  tags = merge(
+    { "Name" = var.name },
+    var.tags,
+    var.default_route_table_tags,
+  )
+}
+
+################################################################################
+# Publiс routes
+################################################################################
+
+resource "aws_route_table" "public" {
+  count = var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = format("%s-${var.public_subnet_suffix}", var.name)
+    },
+    var.tags,
+    var.public_route_table_tags,
+  )
+}
+
+resource "aws_route" "public_internet_gateway" {
+  count = var.create_vpc && var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
+
+  route_table_id         = aws_route_table.public[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this[0].id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+resource "aws_route" "public_internet_gateway_ipv6" {
+  count = var.create_vpc && var.create_igw && var.enable_ipv6 && length(var.public_subnets) > 0 ? 1 : 0
+
+  route_table_id              = aws_route_table.public[0].id
+  destination_ipv6_cidr_block = "::/0"
+  gateway_id                  = aws_internet_gateway.this[0].id
+}
+
+################################################################################
+# Private routes
+# There are as many routing tables as the number of NAT gateways
+################################################################################
+
+resource "aws_route_table" "private" {
+  count = var.create_vpc && local.max_subnet_length > 0 ? local.nat_gateway_count : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = var.single_nat_gateway ? "${var.name}-${var.private_subnet_suffix}" : format(
+        "%s-${var.private_subnet_suffix}-%s",
+        var.name,
+        element(var.azs, count.index),
+      )
+    },
+    var.tags,
+    var.private_route_table_tags,
+  )
+}
+
+################################################################################
+# Connect routes
+# There are as many routing tables as the number of NAT gateways
+################################################################################
+
+resource "aws_route_table" "connect" {
+  count = var.create_vpc && local.max_subnet_length > 0 ? 1 : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = var.single_nat_gateway ? "${var.name}-${var.connect_subnet_suffix}" : format(
+        "%s-${var.connect_subnet_suffix}-%s",
+        var.name,
+        element(var.azs, count.index),
+      )
+    },
+    var.tags,
+    var.connect_route_table_tags,
+  )
+}
+
+
+################################################################################
+# Database routes
+################################################################################
+
+resource "aws_route_table" "database" {
+  count = var.create_vpc && var.create_database_subnet_route_table && length(var.database_subnets) > 0 ? var.single_nat_gateway || var.create_database_internet_gateway_route ? 1 : length(var.database_subnets) : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = var.single_nat_gateway || var.create_database_internet_gateway_route ? "${var.name}-${var.database_subnet_suffix}" : format(
+        "%s-${var.database_subnet_suffix}-%s",
+        var.name,
+        element(var.azs, count.index),
+      )
+    },
+    var.tags,
+    var.database_route_table_tags,
+  )
+}
+
+resource "aws_route" "database_internet_gateway" {
+  count = var.create_vpc && var.create_igw && var.create_database_subnet_route_table && length(var.database_subnets) > 0 && var.create_database_internet_gateway_route && false == var.create_database_nat_gateway_route ? 1 : 0
+
+  route_table_id         = aws_route_table.database[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this[0].id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+resource "aws_route" "database_nat_gateway" {
+  count = var.create_vpc && var.create_database_subnet_route_table && length(var.database_subnets) > 0 && false == var.create_database_internet_gateway_route && var.create_database_nat_gateway_route && var.enable_nat_gateway ? var.single_nat_gateway ? 1 : length(var.database_subnets) : 0
+
+  route_table_id         = element(aws_route_table.database.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.this.*.id, count.index)
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+resource "aws_route" "database_ipv6_egress" {
+  count = var.create_vpc && var.create_egress_only_igw && var.enable_ipv6 && var.create_database_subnet_route_table && length(var.database_subnets) > 0 && var.create_database_internet_gateway_route ? 1 : 0
+
+  route_table_id              = aws_route_table.database[0].id
+  destination_ipv6_cidr_block = "::/0"
+  egress_only_gateway_id      = aws_egress_only_internet_gateway.this[0].id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+################################################################################
+# Redshift routes
+################################################################################
+
+resource "aws_route_table" "redshift" {
+  count = var.create_vpc && var.create_redshift_subnet_route_table && length(var.redshift_subnets) > 0 ? 1 : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = "${var.name}-${var.redshift_subnet_suffix}"
+    },
+    var.tags,
+    var.redshift_route_table_tags,
+  )
+}
+
+################################################################################
+# Elasticache routes
+################################################################################
+
+resource "aws_route_table" "elasticache" {
+  count = var.create_vpc && var.create_elasticache_subnet_route_table && length(var.elasticache_subnets) > 0 ? 1 : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = "${var.name}-${var.elasticache_subnet_suffix}"
+    },
+    var.tags,
+    var.elasticache_route_table_tags,
+  )
+}
+
+################################################################################
+# Intra routes
+################################################################################
+
+resource "aws_route_table" "intra" {
+  count = var.create_vpc && length(var.intra_subnets) > 0 ? 1 : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = "${var.name}-${var.intra_subnet_suffix}"
+    },
+    var.tags,
+    var.intra_route_table_tags,
+  )
+}
+
+################################################################################
+# Route table association
+################################################################################
+
+resource "aws_route_table_association" "private" {
+  count = var.create_vpc && length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+
+  subnet_id = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(
+    aws_route_table.private.*.id,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+}
+
+resource "aws_route_table_association" "connect" {
+  count = var.create_vpc && length(var.connect_subnets) > 0 ? length(var.connect_subnets) : 0
+
+  subnet_id = element(aws_subnet.connect.*.id, count.index)
+  route_table_id = element(
+    aws_route_table.connect.*.id,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+}
+
+resource "aws_route_table_association" "outpost" {
+  count = var.create_vpc && length(var.outpost_subnets) > 0 ? length(var.outpost_subnets) : 0
+
+  subnet_id = element(aws_subnet.outpost.*.id, count.index)
+  route_table_id = element(
+    aws_route_table.private.*.id,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+}
+
+resource "aws_route_table_association" "database" {
+  count = var.create_vpc && length(var.database_subnets) > 0 ? length(var.database_subnets) : 0
+
+  subnet_id = element(aws_subnet.database.*.id, count.index)
+  route_table_id = element(
+    coalescelist(aws_route_table.database.*.id, aws_route_table.private.*.id),
+    var.create_database_subnet_route_table ? var.single_nat_gateway || var.create_database_internet_gateway_route ? 0 : count.index : count.index,
+  )
+}
+
+resource "aws_route_table_association" "redshift" {
+  count = var.create_vpc && length(var.redshift_subnets) > 0 && false == var.enable_public_redshift ? length(var.redshift_subnets) : 0
+
+  subnet_id = element(aws_subnet.redshift.*.id, count.index)
+  route_table_id = element(
+    coalescelist(aws_route_table.redshift.*.id, aws_route_table.private.*.id),
+    var.single_nat_gateway || var.create_redshift_subnet_route_table ? 0 : count.index,
+  )
+}
+
+resource "aws_route_table_association" "redshift_public" {
+  count = var.create_vpc && length(var.redshift_subnets) > 0 && var.enable_public_redshift ? length(var.redshift_subnets) : 0
+
+  subnet_id = element(aws_subnet.redshift.*.id, count.index)
+  route_table_id = element(
+    coalescelist(aws_route_table.redshift.*.id, aws_route_table.public.*.id),
+    var.single_nat_gateway || var.create_redshift_subnet_route_table ? 0 : count.index,
+  )
+}
+
+resource "aws_route_table_association" "elasticache" {
+  count = var.create_vpc && length(var.elasticache_subnets) > 0 ? length(var.elasticache_subnets) : 0
+
+  subnet_id = element(aws_subnet.elasticache.*.id, count.index)
+  route_table_id = element(
+    coalescelist(
+      aws_route_table.elasticache.*.id,
+      aws_route_table.private.*.id,
+    ),
+    var.single_nat_gateway || var.create_elasticache_subnet_route_table ? 0 : count.index,
+  )
+}
+
+resource "aws_route_table_association" "intra" {
+  count = var.create_vpc && length(var.intra_subnets) > 0 ? length(var.intra_subnets) : 0
+
+  subnet_id      = element(aws_subnet.intra.*.id, count.index)
+  route_table_id = element(aws_route_table.intra.*.id, 0)
+}
+
+resource "aws_route_table_association" "public" {
+  count = var.create_vpc && length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
+
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public[0].id
+}
---
+++ _vpn.tf
@@ -0,0 +1,86 @@
+################################################################################
+# Customer Gateways
+################################################################################
+
+resource "aws_customer_gateway" "this" {
+  for_each = var.customer_gateways
+
+  bgp_asn     = each.value["bgp_asn"]
+  ip_address  = each.value["ip_address"]
+  device_name = lookup(each.value, "device_name", null)
+  type        = "ipsec.1"
+
+  tags = merge(
+    {
+      Name = format("%s-%s", var.name, each.key)
+    },
+    var.tags,
+    var.customer_gateway_tags,
+  )
+}
+
+################################################################################
+# VPN Gateway
+################################################################################
+
+resource "aws_vpn_gateway" "this" {
+  count = var.create_vpc && var.enable_vpn_gateway ? 1 : 0
+
+  vpc_id            = local.vpc_id
+  amazon_side_asn   = var.amazon_side_asn
+  availability_zone = var.vpn_gateway_az
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags,
+    var.vpn_gateway_tags,
+  )
+}
+
+resource "aws_vpn_gateway_attachment" "this" {
+  count = var.vpn_gateway_id != "" ? 1 : 0
+
+  vpc_id         = local.vpc_id
+  vpn_gateway_id = var.vpn_gateway_id
+}
+
+resource "aws_vpn_gateway_route_propagation" "public" {
+  count = var.create_vpc && var.propagate_public_route_tables_vgw && (var.enable_vpn_gateway || var.vpn_gateway_id != "") ? 1 : 0
+
+  route_table_id = element(aws_route_table.public.*.id, count.index)
+  vpn_gateway_id = element(
+    concat(
+      aws_vpn_gateway.this.*.id,
+      aws_vpn_gateway_attachment.this.*.vpn_gateway_id,
+    ),
+    count.index,
+  )
+}
+
+resource "aws_vpn_gateway_route_propagation" "private" {
+  count = var.create_vpc && var.propagate_private_route_tables_vgw && (var.enable_vpn_gateway || var.vpn_gateway_id != "") ? length(var.private_subnets) : 0
+
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+  vpn_gateway_id = element(
+    concat(
+      aws_vpn_gateway.this.*.id,
+      aws_vpn_gateway_attachment.this.*.vpn_gateway_id,
+    ),
+    count.index,
+  )
+}
+
+resource "aws_vpn_gateway_route_propagation" "intra" {
+  count = var.create_vpc && var.propagate_intra_route_tables_vgw && (var.enable_vpn_gateway || var.vpn_gateway_id != "") ? length(var.intra_subnets) : 0
+
+  route_table_id = element(aws_route_table.intra.*.id, count.index)
+  vpn_gateway_id = element(
+    concat(
+      aws_vpn_gateway.this.*.id,
+      aws_vpn_gateway_attachment.this.*.vpn_gateway_id,
+    ),
+    count.index,
+  )
+}
